82cb70c4f70f5300a82df99197d5884f7b11c77c
Good IO <antonaavik@gmail.com>
2025-05-11 17:37:35 +0300

    multi-form: hold values onBackButton

--- src/solution/templates/MultiForm/MultiFormTemplate.tsx
+++ src/solution/templates/MultiForm/MultiFormTemplate.tsx
@@ -1,13 +1,16 @@
+import {  useFormContext } from "react-hook-form";
+
 interface MultiFormTemplateProps {
   children: React.ReactNode;
   meta?: Record<string, unknown>;
 }
 
 export function MultiFormTemplate({ children, meta }: MultiFormTemplateProps) {
+  const { getValues} = useFormContext();
   const activeStepIndex = meta?.activeStepIndex;
   const isFirstStep = meta?.isFirstStep;
   const isLastStep = meta?.isLastStep;
-  const onPrevStep = meta?.onPrevStep as () => void;
+  const onPrevStep = meta?.onPrevStep as (values: Record<string, unknown>) => void;
 
   return (
     <div className="multiform-tpl">
@@ -15,7 +18,7 @@ export function MultiFormTemplate({ children, meta }: MultiFormTemplateProps) {
         {children}
       </div>
       <div className="signup-tpl__footer">
-        {!isFirstStep && <button type="button" onClick={() => onPrevStep?.()}>Previous</button>}
+        {!isFirstStep && <button type="button" onClick={() => onPrevStep?.(getValues())}>Previous</button>}
         <button type="submit">Submit</button>
       </div>
     </div>
